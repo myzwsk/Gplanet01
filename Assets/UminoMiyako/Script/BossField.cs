740165d9cccaedcdcfc749cc59c2b919cbc7dc37
uminomiyako0225 <24ci0225@jec.ac.jp>
2025-11-25 15:00:58 +0900

    ボス準備　並木

---
+++ Assets/UminoMiyako/Script/BossField.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class BossField : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (false)
+        {
+            ObjectFalse();
+        }
+        else if (false)
+        {
+            ObjectTrue();
+        }
+    }
+    void ObjectTrue()
+    {
+        GetComponent<Collider>().enabled = true;
+        GetComponent<Renderer>().enabled = true;
+    }
+    void ObjectFalse()
+    {
+        GetComponent<Collider>().enabled = false;
+        GetComponent<Renderer>().enabled = false;
+    }
+}
