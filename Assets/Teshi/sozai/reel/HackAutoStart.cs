789e2793fe121b111c06672ae51335bda027aa1f
CI <CI@jec.ac.jp>
2025-12-15 16:40:59 +0900

    ミニゲーム2

---
+++ Assets/Teshi/sozai/reel/HackAutoStart.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class HackAutoStart : MonoBehaviour
+{
+    public GameObject hackCanvas;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        hackCanvas.SetActive(true);   // Playした瞬間に表示
+        Time.timeScale = 0f;          // ゲームを止める（ハッキング中）
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
