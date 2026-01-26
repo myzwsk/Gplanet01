279a8b78a4306ab59b8e617f8f46c49814b998b3
CI <CI@jec.ac.jp>
2026-01-26 16:22:20 +0900

    風呂追加　勅使

---
+++ Assets/Teshi/sozai/main/BathRiseController..cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BathRiseController : MonoBehaviour
+{
+    public Transform riseGroup;   // BathRiseGroup
+    public float startY = -2f;
+    public float endY = 0f;
+    public float riseSpeed = 0.5f;
+
+    bool isRising = false;
+
+    public void StartRise()
+    {
+        if (isRising) return;
+        StartCoroutine(RiseRoutine());
+    }
+
+    IEnumerator RiseRoutine()
+    {
+        isRising = true;
+
+        Vector3 pos = riseGroup.position;
+        pos.y = startY;
+        riseGroup.position = pos;
+
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += riseSpeed * Time.unscaledDeltaTime;
+            t = Mathf.Clamp01(t);
+
+            pos.y = Mathf.Lerp(startY, endY, t);
+            riseGroup.position = pos;
+
+            yield return null;
+        }
+
+        isRising = false;
+    }
+}
