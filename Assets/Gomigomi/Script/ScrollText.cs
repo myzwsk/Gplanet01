91734f8d11c93ecdfab0b58921c1226f62a74441
myzwsk <24ci0232@jec.ac.jp>
2026-01-28 16:41:01 +0900

    リザルト　宮澤

---
+++ Assets/Gomigomi/Script/ScrollText.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using TMPro;
+public class ScrollText : MonoBehaviour
+{
+    public float scrollSpeed = -20f;
+    public float startPosition = 500f;  // テキストが動き出す位置
+    public float resetPosition = -500f; // テキストが消えきる位置
+
+    private RectTransform rectTransform;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 上方向に移動
+        rectTransform.anchoredPosition += new Vector2(0, scrollSpeed * Time.deltaTime);
+
+        // 一定位置まで行ったらループ（必要に応じて）
+        if (rectTransform.anchoredPosition.y > resetPosition)
+        {
+            rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, startPosition);
+        }
+
+    }
+}
