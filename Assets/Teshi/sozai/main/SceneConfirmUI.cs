48aa7af10c4402fb80f71ea4e7faa59f60c9e794
CI <CI@jec.ac.jp>
2026-02-02 15:11:57 +0900

    風呂追加　勅使

---
+++ Assets/Teshi/sozai/main/SceneConfirmUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneConfirmUI : MonoBehaviour
+{
+    public GameObject panel;
+    public string nextSceneName;
+
+    public void Open()
+    {
+        panel.SetActive(true);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void OnYes()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+
+    public void OnNo()
+    {
+        panel.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
---
+++ Assets/Teshi/sozai/main/SceneTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SceneTrigger : MonoBehaviour
+{
+    public SceneConfirmUI ui;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ui.Open();
+        }
+    }
+}
